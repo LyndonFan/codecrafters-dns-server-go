5693f4791469bca75f80384c9fef9f0540ecdfc1
Lyndon Fan <lyndon0808@gmail.com>
2023-11-20 20:31:10 +0000

    test 2nd stage

--- app/main.go
+++ app/main.go
@@ -39,10 +39,47 @@ func main() {
 
 		// Create an empty response
 		response := []byte{}
+		header := calculateHeaders()
+		response = append(response, header...)
 
 		_, err = udpConn.WriteToUDP(response, source)
 		if err != nil {
 			fmt.Println("Failed to send response:", err)
 		}
 	}
+}
+
+func calculateHeaders() []byte {
+	header := make([]byte, 0, 12)
+	identifier := 1234
+	header = append(header, byte(identifier>>8), byte(identifier&0xff)) // 1234 = 0x04d2
+
+	qrIndicator := 1
+	oopCode := 0
+	authoritativeAnswer := 0
+	truncation := 0
+	recursionDesired := 0
+	val := (qrIndicator << 7) | (oopCode << 3)
+	val = val | (authoritativeAnswer << 2) | (truncation << 1) | recursionDesired
+	header = append(header, byte(val))
+
+	recursionAvailable := 0
+	reserved := 0
+	responseCode := 0
+	val = (recursionAvailable << 7) | (reserved << 4) | responseCode
+	header = append(header, byte(val))
+
+	questionCount := 0
+	header = append(header, byte(questionCount>>8), byte(questionCount&0xff))
+
+	answerRecordCount := 0
+	header = append(header, byte(answerRecordCount>>8), byte(answerRecordCount&0xff))
+
+	authorityRecordCount := 0
+	header = append(header, byte(authorityRecordCount>>8), byte(authorityRecordCount&0xff))
+
+	additionalRecordCount := 0
+	header = append(header, byte(additionalRecordCount>>8), byte(additionalRecordCount&0xff))
+
+	return header
 }
