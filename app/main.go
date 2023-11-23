81f1a3bc14d9560e9866729a335cf84b1ced36c6
Lyndon Fan <lyndon0808@gmail.com>
2023-11-23 11:18:03 +0000

    feat: parse parload header

--- app/main.go
+++ app/main.go
@@ -2,67 +2,9 @@ package main
 
 import (
 	"fmt"
-	"strings"
-
-	// Uncomment this block to pass the first stage
 	"net"
+	"strings"
 )
-
-type Header struct {
-	Identifier            uint16
-	QR                    bool
-	OpCode                uint8
-	Authoritative         bool
-	Truncation            bool
-	RecursionDesired      bool
-	RecursionAvailable    bool
-	Reserved              uint8
-	ResponseCode          uint8
-	QuestionCount         uint16
-	AnswerRecordCount     uint16
-	AuthorityRecordCount  uint16
-	AdditionalRecordCount uint16
-}
-
-func (h Header) AsBytes() []byte {
-	res := make([]byte, 12)
-	res[0] = byte(h.Identifier >> 8)
-	res[1] = byte(h.Identifier & 0xff)
-
-	if h.QR {
-		res[2] |= 1 << 7
-	}
-	res[2] |= uint8(h.OpCode) << 3
-	if h.Authoritative {
-		res[2] |= 1 << 2
-	}
-	if h.Truncation {
-		res[2] |= 1 << 1
-	}
-	if h.RecursionDesired {
-		res[2] |= 1
-	}
-
-	if h.RecursionAvailable {
-		res[3] |= 1 << 7
-	}
-	res[3] |= h.Reserved << 4
-	res[3] |= h.ResponseCode
-
-	res[4] = byte(h.QuestionCount >> 8)
-	res[5] = byte(h.QuestionCount & 0xff)
-
-	res[6] = byte(h.AnswerRecordCount >> 8)
-	res[7] = byte(h.AnswerRecordCount & 0xff)
-
-	res[8] = byte(h.AuthorityRecordCount >> 8)
-	res[9] = byte(h.AuthorityRecordCount & 0xff)
-
-	res[10] = byte(h.AdditionalRecordCount >> 8)
-	res[11] = byte(h.AdditionalRecordCount & 0xff)
-
-	return res
-}
 
 type Question struct {
 	Name  string
@@ -119,11 +61,8 @@ func (a Answer) AsBytes() []byte {
 }
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
 	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
 	if err != nil {
 		fmt.Println("Failed to resolve UDP address:", err)
@@ -166,11 +105,21 @@ func main() {
 			RDData:   []byte{0x08, 0x08, 0x08, 0x08},
 		})
 
+		payloadHeader := Header{}
+		payloadHeader.FromBytes(buf[:12])
+
 		header := Header{
-			Identifier:        1234,
+			Identifier:        payloadHeader.Identifier,
 			QR:                true,
+			OpCode:            payloadHeader.OpCode,
+			RecursionDesired:  payloadHeader.RecursionDesired,
 			QuestionCount:     uint16(len(questions)),
 			AnswerRecordCount: uint16(len(answers)),
+		}
+		if payloadHeader.OpCode == 0x00 {
+			header.ResponseCode = 0x00
+		} else {
+			header.ResponseCode = 0x04
 		}
 
 		response := make([]byte, 0, 512)
